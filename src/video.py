ab310b156ac9a2bc0ee964b9560b468a2bd46a01
Erdni <erkotinov@gmail.com>
2023-07-24 16:25:00 +0300

    homework 4 done

--- src/video.py
+++ src/video.py
@@ -1,18 +1,36 @@
+import requests
+import os
+
 class Video:
-    def __init__(self, video_id, title, video_link, views, likes):
+    def __init__(self, video_id):
         self.video_id = video_id
-        self.title = title
-        self.video_link = video_link
-        self.views = views
-        self.likes = likes
+        self._fetch_video_data()
+
+    def _fetch_video_data(self):
+        api_key = os.getenv('API_KEY')  # Replace 'YOUR_API_KEY' with your actual YouTube API key
+        url = f'https://www.googleapis.com/youtube/v3/videos?id={self.video_id}&key={api_key}&part=snippet,statistics'
+        response = requests.get(url)
+        data = response.json()
+
+        if 'items' in data and len(data['items']) > 0:
+            item = data['items'][0]
+            snippet = item['snippet']
+            statistics = item['statistics']
+
+            self.title = snippet['title']
+            self.link = f"https://www.youtube.com/watch?v={self.video_id}"
+            self.views = int(statistics['viewCount'])
+            self.likes = int(statistics['likeCount'])
+        else:
+            raise ValueError(f"Video with ID '{self.video_id}' not found on YouTube.")
 
     def __str__(self):
         return self.title
 
 
 class PLVideo(Video):
-    def __init__(self, video_id, title, video_link, views, likes, playlist_id):
-        super().__init__(video_id, title, video_link, views, likes)
+    def __init__(self, video_id, playlist_id):
+        super().__init__(video_id)
         self.playlist_id = playlist_id
 
     def __str__(self):
